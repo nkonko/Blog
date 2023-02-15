a0f73202fe384ddcc4928b1838bcda60a03acd95
nazzara <nazzara@hexacta.com>
2023-02-15 23:02:04 +0100

    Initial commit

---
+++ BlogAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogAPI.Controllers
+{
+    public class AuthenticationController : BaseApiController
+    {
+
+
+    }
+}
---
+++ TestBlog/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestBlog
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
