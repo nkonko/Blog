a175406a801f894a6c959ae754829ef860469bdd
Nicolas Brosan <nicolasbrosan@hotmail.com>
2023-02-16 23:17:45 -0300

    Se crean las entidades

---
+++ Domain/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    [Table("Comment")]
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Comments { get; set; }//No permite colocar el mismo nombre que la clase
+        public string? Email { get; set; }
+        public int Post_id { get; set; }
+        public DateTime? Created_at { get; set; }
+        public int? Status { get; set; }
+    }
+}
---
+++ Domain/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    [Table("Post")]
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Brief { get; set; }
+        public string? Content { get; set; }
+        public string? Image { get; set; }
+        public DateTime? Created_at { get; set; }
+        public int? Status { get; set; }
+        public int Category_id { get; set; }
+        public int User_id { get; set; }
+    }
+}
