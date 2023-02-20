1bed60fe8ae8968bdd88152f57426e6a8b55abc6
ArielC27 <109997773+ArielC27@users.noreply.github.com>
2023-02-20 16:08:08 -0300

    Add BlogContextFacotry

--- BlogAPI/Program.cs
+++ BlogAPI/Program.cs
@@ -1,18 +1,22 @@
 using BlogAPI;
+using Data.AppContext;
 using Data.InitialDB;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
 
+builder.Services.AddDbContext<BlogContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("Postgre")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -23,12 +27,12 @@ app.MapControllers();
 
 try
 {
-    ////Add admin user to DB if not exists.
-    await InitialDB.SeedUserAsync();
+	////Add admin user to DB if not exists.
+	await InitialDB.SeedUserAsync();
 }
 catch (Exception ex)
 {
-    ////Log exception
+	////Log exception
 }
 
 app.Run();
---
+++ Data/AppContext/BlogContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Data.AppContext
+
+{
+	public class BlogContext : DbContext
+	{
+		public BlogContext(DbContextOptions<BlogContext> options)
+		{
+
+		}
+		public DbSet<User> Users { get; set; }
+	}
+}
