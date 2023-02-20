895959372f9349940d8e34eae5144a5dc692f37f
ArielC27 <109997773+ArielC27@users.noreply.github.com>
2023-02-20 14:49:23 -0300

    Se agrega el DbContext

---
+++ Data/AppContext/AppContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Data.AppContext
+
+{
+	public class AppContext : DbContext
+	{
+		private readonly IConfiguration configuration;
+
+		public AppContext(IConfiguration configuration)
+		{
+			this.configuration = configuration;
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+			options.UseNpgsql(this.configuration.GetConnectionString("Postgre"));
+		}
+
+		public DbSet<User> Users { get; set; }
+	}
+}
