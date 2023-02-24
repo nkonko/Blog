a7d0e36540df6afe4b14627dff47e0f80bca4364
ArielC27 <109997773+ArielC27@users.noreply.github.com>
2023-02-23 22:03:42 -0300

    Add Generic Repository

---
+++ Services/GenericRepository/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Context;
+
+namespace Services.GenericRepository
+{
+	public class GenericRepository<T> : IGenericRepository<T> where T : class
+	{
+		private readonly BlogContext blogContext;
+		public async Task<T> Delete(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IEnumerable<T>> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<T> GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<T> Insert(T entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<T> Update(T entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
