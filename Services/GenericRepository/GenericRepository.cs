351d6214bcd11875942bdecdb89616d2bbd50916
ArielC27 <109997773+ArielC27@users.noreply.github.com>
2023-02-23 22:08:27 -0300

    Pepe

---
+++ Services/GenericRepository/GenericRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.GenericRepository
+{
+	public class GenericRepository<T> : IGenericRepository<T> where T : class
+	{
+		public async Task<T> Delete(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IEnumerable<T>> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<T> GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<T> Insert(T entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<T> Update(T entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Services/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Services.GenericRepository
+{
+	public interface IGenericRepository<T> where T : class
+	{
+		Task<IEnumerable<T>> GetAll();
+		Task<T> GetById(int id);
+		Task<T> Insert(T entity);
+		Task<T> Update(T entity);
+		Task<T> Delete(int id);
+	}
+}
