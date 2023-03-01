72f64bebb6770bf6be6da9072fb03f47184a0e54
ArielC27 <109997773+ArielC27@users.noreply.github.com>
2023-03-01 20:56:39 -0300

    Changes Repo

--- Services/GenericRepository/IGenericRepository.cs
+++ Services/GenericRepository/IGenericRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain;
+﻿using Domain;
 
 namespace Services.GenericRepository
 {
@@ -14,8 +9,5 @@ namespace Services.GenericRepository
 		void Delete(T entity);
 		Task<IReadOnlyList<T>> GetAll();
 		Task<T> GetById(int id);
-		Task<T> Insert(T entity);
-		Task<T> Update(T entity);
-		Task<T> Delete(int id);
 	}
 }
