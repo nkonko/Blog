90e736bd93c6a6646bbb73c59510b46b68e2d8bb
ArielC27 <109997773+ArielC27@users.noreply.github.com>
2023-03-01 20:51:44 -0300

    Adder changes repository

---
+++ Services/GenericRepository/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.AppContext;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services.GenericRepository
+{
+	public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+	{
+		private readonly BlogContext blogContext;
+
+		public GenericRepository(BlogContext blogContext)
+		{
+			this.blogContext = blogContext;
+		}
+		public void Add(T entity)
+		{
+			this.blogContext.Set<T>().Add(entity);
+		}
+
+		public void Delete(T entity)
+		{
+			this.blogContext.Set<T>().Remove(entity);
+		}
+
+		public async Task<IReadOnlyList<T>> GetAll()
+		{
+			return await this.blogContext.Set<T>().ToListAsync();
+		}
+
+		public async Task<T> GetById(int id)
+		{
+			return await this.blogContext.Set<T>().FindAsync(id);
+		}
+
+		public void Update(T entity)
+		{
+			this.blogContext.Set<T>().Attach(entity);
+			this.blogContext.Entry(entity).State = EntityState.Modified;
+		}
+	}
+}
--- Services/GenericRepository/IGenericRepository.cs
+++ Services/GenericRepository/IGenericRepository.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain;
 
 namespace Services.GenericRepository
 {
-	public interface IGenericRepository
+	public interface IGenericRepository<T> where T : BaseEntity
 	{
-
+		void Add(T entity);
+		void Update(T entity);
+		void Delete(T entity);
+		Task<IReadOnlyList<T>> GetAll();
+		Task<T> GetById(int id);
 	}
 }
