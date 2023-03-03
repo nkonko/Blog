267fc765c8a1ad2357422de25f1595e8fa645c7f
ArielC27 <109997773+ArielC27@users.noreply.github.com>
2023-03-02 21:39:19 -0300

    Spaces

--- Services/GenericRepository/IGenericRepository.cs
+++ Services/GenericRepository/IGenericRepository.cs
@@ -5,9 +5,13 @@ namespace Services.GenericRepository
 	public interface IGenericRepository<T> where T : BaseEntity
 	{
 		void Add(T entity);
+
 		void Update(T entity);
+
 		void Delete(T entity);
+
 		IReadOnlyList<T> GetAll();
+
 		T GetById(int id);
 	}
 }
